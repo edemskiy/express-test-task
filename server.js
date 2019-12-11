7e542cb32309cfa0d3a1b1d901f438e3daff7acd
edemskiy <qapple@ya.ru>
2019-12-11 16:57:56 +0300

    update port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const foremansRouter = require("./src/js/routes/foremans");
 
 require("dotenv").config();
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
